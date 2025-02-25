b583e0d560667c685db0b94fda6d0676f65e719b
BREstivenGn <begomez1054@gmail.com>
2025-02-25 09:56:45 -0500

    Implementacion de boton de con animacion

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,35 @@
-import products from '@/assets/data/products';
-import Colors from '@/constants/Colors';
-import { Text, View } from '@/components/Themed';
-import Button from '@/components/Button';
-import { Image,  } from 'react-native';
-import NotificationLocal from '../../components/NotificationLocal';
+import products from "@/assets/data/products";
+import Colors from "@/constants/Colors";
+import { Text, View } from "@/components/Themed";
+import Button from "@/components/Button";
+import { Image } from "react-native";
+import NotificationLocal from "../../components/NotificationLocal";
+import AnimationTextInput from "../../components/animation-text-input/Index";
 
 const product = products[1];
 export default function TabOneScreen() {
-
   return (
     <>
-    <NotificationLocal></NotificationLocal>
-    <View className='items-center justify-center p-3 '>
-      <Image source={{ uri: product.image }} className='w-full aspect-[1]' />
+      <View className="items-center justify-center p-3 ">
+        <AnimationTextInput placeholder="Nombre" 
+        activeColor={Colors.light.tint}
+        />
+        <Image source={{ uri: product.image }} className="w-full aspect-[1]" />
 
-      <Text className='text-lg my-5 font-bold'>{product.name}</Text>
-      <Text className='text-sm font-[600]' style={{color: Colors.light.tint}}>{product.price}</Text>
+        <Text className="text-lg my-5 font-bold">{product.name}</Text>
+        <Text
+          className="text-sm font-[600]"
+          style={{ color: Colors.light.tint }}
+        >
+          {product.price}
+        </Text>
 
-      <Button className='bg-black'>
-        <Text className=' font-bold' style={{color: Colors.light.tint}}>Add to Cart</Text>
-      </Button>
-    </View>
+        <Button className="bg-black">
+          <Text className=" font-bold" style={{ color: Colors.light.tint }}>
+            Add to Cart
+          </Text>
+        </Button>
+      </View>
     </>
   );
 }
-
